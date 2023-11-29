fb40a7b5f2e2efe811169d6179385c4d3590f6a4
JeYeongR <wpdud2003@gmail.com>
2023-11-29 20:41:35 +0900

    Fix: 유저 엔티티 컬럼 nullable 수정

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -7,28 +7,41 @@ export class User extends Common {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 8 })
+  @Column({
+    length: 8,
+    nullable: true,
+  })
   nickname: string;
 
   @Column({ length: 30 })
   email: string;
 
-  @Column()
+  @Column({ nullable: true })
   height: number;
 
-  @Column({ name: "goal_weight" })
+  @Column({
+    name: "goal_weight",
+    nullable: true,
+  })
   goalWeight: number;
 
-  @Column({ name: "goal_body_fat" })
-  goalBodyFat: number;
-
-  @Column({ name: "goal_skeletal_muscle_mass" })
+  @Column({
+    name: "goal_skeletal_muscle_mass",
+    nullable: true,
+  })
   goalSkeletalMuscleMass: number;
 
-  @Column({ name: "birth_year", length: 10 })
+  @Column({
+    name: "birth_year",
+    length: 10,
+    nullable: true,
+  })
   birthYear: string;
 
-  @Column({ name: "social_id", length: 50 })
+  @Column({
+    name: "social_id",
+    length: 50,
+  })
   socialId: string;
 
   @Column()
