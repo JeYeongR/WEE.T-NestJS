b9b36565b18ddca900c64a40c6c61768bd7099fe
JeYeongR <wpdud2003@gmail.com>
2023-12-03 01:48:46 +0900

    Test: UserService findUserByEmail 단위 테스트 추가

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -29,15 +29,15 @@ describe("UserService", () => {
     userRepository = module.get(getRepositoryToken(User));
   });
 
-  const email = "test@test.com";
-  const provider = "naver";
-  const socialId = "test1234";
-  const user = {
+  const email: string = "test@test.com";
+  const provider: string = "naver";
+  const socialId: string = "test1234";
+  const user: Partial<User> = {
     email,
     provider,
     socialId,
   };
-  const mockedUser = {
+  const mockedUser: User = {
     email,
     socialId,
     provider,
@@ -47,9 +47,10 @@ describe("UserService", () => {
     goalWeight: null,
     goalSkeletalMuscleMass: null,
     birthYear: null,
-    createdAt: "2023-12-02T12:54:59.566Z",
-    updatedAt: "2023-12-02T12:54:59.566Z",
+    createdAt: new Date("2023-12-02T12:54:59.566Z"),
+    updatedAt: new Date("2023-12-02T12:54:59.566Z"),
     id: 1,
+    gender: null,
   };
 
   it("should be defined", () => {
@@ -72,6 +73,34 @@ describe("UserService", () => {
       expect(userRepository.create).toHaveBeenCalledWith({ email, provider, socialId });
       expect(userRepository.save).toHaveBeenCalledTimes(1);
       expect(userRepository.save).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe("findUserByEmail()", () => {
+    it("should be found user", async () => {
+      // Given
+      userRepository.findOneBy.mockResolvedValue(mockedUser);
+
+      // When
+      const result = await userService.findUserByEmail(email);
+
+      // Then
+      expect(result).toEqual(mockedUser);
+      expect(userRepository.findOneBy).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOneBy).toHaveBeenCalledWith({ email: email });
+    });
+
+    it("should not be found user", async () => {
+      // Given
+      userRepository.findOneBy.mockResolvedValue(null);
+
+      // When
+      const result = await userService.findUserByEmail(email);
+
+      // Then
+      expect(result).toBeNull();
+      expect(userRepository.findOneBy).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOneBy).toHaveBeenCalledWith({ email: email });
     });
   });
 });
